be352bcbd68869f329bde009f6d654c51aa12488
Emmanuel Nicolau <emmanicolau@gmail.com>
2018-06-25 12:47:36 -0300

    Save rates in a transaction

--- lib/danconia/stores/active_record.rb
+++ lib/danconia/stores/active_record.rb
@@ -2,8 +2,10 @@ module Danconia
   module Stores
     class ActiveRecord
       def save_rates rates
-        rates.each do |pair, rate|
-          ExchangeRate.where(pair: pair).first_or_initialize.update rate: rate
+        ExchangeRate.transaction do
+          rates.each do |pair, rate|
+            ExchangeRate.where(pair: pair).first_or_initialize.update rate: rate
+          end
         end
       end
 
