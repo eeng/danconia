ef4d2bb2e8aad4640ce48ece27d7883346c91b8f
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-09-01 16:04:11 -0300

    Refactor some tests

--- lib/danconia/version.rb
+++ lib/danconia/version.rb
@@ -1,3 +1,3 @@
 module Danconia
-  VERSION = '0.3.0'
+  VERSION = '0.2.9'
 end
--- spec/danconia/exchanges/currency_layer_spec.rb
+++ spec/danconia/exchanges/currency_layer_spec.rb
@@ -7,29 +7,12 @@ module Danconia
 
       context 'fetch_rates' do
         it 'uses the API to retrive the rates' do
-          stub_request(:get, 'http://www.apilayer.net/api/live?access_key=[KEY]').to_return body: <<~END
-            {
-                "success": true,
-                "source": "USD",
-                "quotes": {
-                    "USDARS": 27.110001,
-                    "USDAUD": 1.346196
-                }
-            }
-          END
+          stub_request(:get, 'http://www.apilayer.net/api/live?access_key=[KEY]').to_return body: fixture('success.json')
           expect(subject.fetch_rates).to eq 'USDARS' => 27.110001, 'USDAUD' => 1.346196
         end
 
         it 'when the API returns an error' do
-          stub_request(:get, 'http://www.apilayer.net/api/live?access_key=[KEY]').to_return body: <<~END
-            {
-                "success": false,
-                "error": {
-                  "code": 104,
-                  "info": "Your monthly usage limit has been reached. Please upgrade your subscription plan."
-                }
-            }
-          END
+          stub_request(:get, 'http://www.apilayer.net/api/live?access_key=[KEY]').to_return body: fixture('failure.json')
           expect { subject.fetch_rates }.to raise_error Errors::APIError
         end
       end
@@ -49,6 +32,10 @@ module Danconia
           subject.update_rates!
           expect(subject.rate('USD', 'ARS')).to eq 3.1
         end
+      end
+
+      def fixture file
+        File.read("#{__dir__}/fixtures/currency_layer/#{file}")
       end
     end
   end
